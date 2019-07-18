4ff1f4d1d388bf10e79b384b102344e10113df21
Samy Hussaein <samy.hussaein@epita.fr>
2019-07-18 14:26:00 +0200

    [LOG] Added a log system

---
+++ log.py
@@ -0,0 +1,23 @@
+import enum
+from datetime import datetime
+
+from colorama import Fore as Foreground, init
+
+
+def critical(message):
+    init(autoreset=True)
+    print(Foreground.MAGENTA, "[" + str(datetime.now()) + "] " + message, flush=True)
+
+
+def error(message):
+    init(autoreset=True)
+    print(Foreground.RED, "[" + str(datetime.now()) + "] " + message, flush=True)
+
+
+def warning(message):
+    init(autoreset=True)
+    print(Foreground.YELLOW, "[" + str(datetime.now()) + "] " + message, flush=True)
+
+
+def info(message):
+    print("[" + str(datetime.now()) + "] " + message, flush=True)
